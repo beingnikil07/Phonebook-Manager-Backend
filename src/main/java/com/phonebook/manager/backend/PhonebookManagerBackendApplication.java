c743a47346a54cb54ef44dc53fa4dcf418c5cc07
nikhil <kumarnikil220@gmail.com>
2025-02-13 20:35:20 +0530

    Setting up the project

---
+++ src/main/java/com/phonebook/manager/backend/PhonebookManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phonebook.manager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookManagerBackendApplication {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PhonebookManagerBackendApplication.class, args);
+	}
+
+}
