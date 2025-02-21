f72e36b44c7e4d20cf4add18cbfad748b92c5195
nikhil <kumarnikil220@gmail.com>
2025-02-21 15:47:39 +0530

    Exposing the contacts api endpoints

--- src/main/java/com/phonebook/manager/backend/repository/ContactRepository.java
+++ src/main/java/com/phonebook/manager/backend/repository/ContactRepository.java
@@ -2,6 +2,13 @@ package com.phonebook.manager.backend.repository;
 
 import com.phonebook.manager.backend.Entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path ="contacts",collectionResourceRel ="contacts")
 public interface ContactRepository extends JpaRepository<Contact,String> {
+
+    Contact findByEmail(String email);
+
+    Contact findByPhoneNumber(String phoneNumber);
+
 }
