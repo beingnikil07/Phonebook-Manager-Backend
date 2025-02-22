46d2300baa14823e55cabe0514ae881ba90141d9
nikhil <kumarnikil220@gmail.com>
2025-02-22 15:01:08 +0530

    returning apis in plain json format

--- src/main/java/com/phonebook/manager/backend/config/RestConfig.java
+++ src/main/java/com/phonebook/manager/backend/config/RestConfig.java
@@ -1,9 +1,13 @@
 package com.phonebook.manager.backend.config;
 
+import com.phonebook.manager.backend.Entity.Contact;
+import com.phonebook.manager.backend.Entity.User;
+import com.phonebook.manager.backend.constant.AppConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
@@ -13,8 +17,10 @@ public class RestConfig {
             return new RepositoryRestConfigurer() {
                 @Override
                 public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-                        config.setBasePath("/api");
-                        config.setDefaultPageSize(20);
+                        config.setBasePath(AppConstants.API_BASEPATH);
+                        config.setDefaultPageSize(AppConstants.PAGE_SIZE);
+                        config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+                        config.exposeIdsFor(User.class, Contact.class);
                 }
             };
     }
---
+++ src/main/java/com/phonebook/manager/backend/constant/AppConstants.java
@@ -0,0 +1,7 @@
+package com.phonebook.manager.backend.constant;
+
+public class AppConstants {
+    public static final String API_BASEPATH="/api";
+    public static final int PAGE_SIZE=20;
+
+}
